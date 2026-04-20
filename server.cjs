6c1a866acb047a73506d9e07cb4e78658157bc94
bakarbobokhidze <baqarboboxidze@gmail.com>
2026-04-20 13:38:30 +0400

    Refactor allergens field to support multiple languages

--- server.cjs
+++ server.cjs
@@ -25,7 +25,14 @@ const dishSchema = new mongoose.Schema({
   calories: Number,
   prepTime: String,
   badges: [String],
-  allergens: [String],
+  allergens: [
+    {
+      ge: { type: String, default: "" },
+      en: { type: String, default: "" },
+      de: { type: String, default: "" },
+      ru: { type: String, default: "" },
+    }
+  ],,
   portions: [{ label: { en: { type: String, default: "" }, ge: { type: String, default: "" }, de: { type: String, default: "" }, ru: { type: String, default: "" } }, weight: String, price: Number }],
 });
 
